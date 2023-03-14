1f6619459654c1c110281c1cc9ffa75dddc13b9e
merpmerpmerp <91646770+merpmerpmerp@users.noreply.github.com>
2023-03-13 20:13:16 -0500

    added styling

--- whackamole.js
+++ whackamole.js
@@ -6,6 +6,7 @@ const grid = document.querySelector(".wrapper");
 const background = document.querySelector(".mole-hole");
 const startBtn = document.getElementById("start-button").addEventListener("click", startGame);
 const score = document.querySelector("#score");
+
 let scoreCount = 0;
 score.innerText = scoreCount;
 
@@ -13,10 +14,11 @@ score.innerText = scoreCount;
 function startGame(e){
     
 // set interval ran for the timer function, which is counting down from 10 seconds.
-let timer = 10;
+
+let timer = 30;
 let counter = setInterval(() => {
     if(timer > 0){
-  document.getElementById("timeLeft").innerText = timer;
+  document.getElementById("timeLeft").innerText = "Time Left: " + timer + " seconds";
   timer--; }
 // clears interval of timer and divColor functions and exits the function with return
   else {
@@ -41,12 +43,13 @@ let divColor = setInterval(() => {
 
     setTimeout(() => {
         body.style.backgroundColor = "red";
-    }, 1000);
+    }, 250);
 
-  }, 2000);
+  }, 1000);
 
   // Event listener to the grid variable which leads into the function that tabulates the score feature. When a user
   // clicks the div while it has a random color, the user gains 1 point.
+
 grid.addEventListener("click", (e) => {
     if (e.target.classList.contains("mole")) {
         e.target.classList.remove("mole");
